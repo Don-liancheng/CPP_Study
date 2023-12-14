cdd9b4e006d439361da0582bd72228c4ddcae73f
“包饭厅” <1316149505@qq.com>
2023-12-14 14:13:10 +0800

    定时器

--- QT/QTCode/ThreeDay/02_QtEvent/mainWidget.cpp
+++ QT/QTCode/ThreeDay/02_QtEvent/mainWidget.cpp
@@ -1,10 +1,46 @@
 #include "mainWidget.h"
-
+#include <QTimer>
 mainWidget::mainWidget(QWidget *parent)
     : QWidget(parent)
 {
     ui.setupUi(this);
+
+    //启动定时器
+    id1 = startTimer(1000);//单位毫秒，每隔一秒调用函数
+
+    id2 = startTimer(2000);//单位毫秒，每隔2秒调用函数
+
+	//定时器的第二种方式
+	QTimer* timer = new QTimer(this);
+	//启动定时器
+	timer->start(500);
+
+	connect(timer,&QTimer::timeout,[=](){
+		static int num2 = 1;
+		ui.label3->setText(QString::number(num2++));
+	});
+	//点击按钮暂停
+	connect(ui.btn1,&QPushButton::clicked,[=](){
+		timer->stop();
+	});
 }
 
 mainWidget::~mainWidget()
 {}
+
+void mainWidget::timerEvent(QTimerEvent * event)
+{
+
+	if (event->timerId() == id1)
+	{
+		static int num = 1;
+		ui.label2->setText(QString::number(num++));
+	}
+ //   //两秒跳一次
+	//if (event->timerId() == id2)
+	//{
+	//	static int num2 = 1;
+	//	ui.label3->setText(QString::number(num2++));
+	//}
+    
+}
--- QT/QTCode/ThreeDay/02_QtEvent/mainWidget.h
+++ QT/QTCode/ThreeDay/02_QtEvent/mainWidget.h
@@ -2,6 +2,7 @@
 
 #include <QtWidgets/QWidget>
 #include "ui_mainWidget.h"
+#include <QLabel>
 
 class mainWidget : public QWidget
 {
@@ -10,6 +11,12 @@ class mainWidget : public QWidget
 public:
     mainWidget(QWidget *parent = nullptr);
     ~mainWidget();
+
+    //重写定时器事件
+    void timerEvent(QTimerEvent *event);
+     
+    int id1;//定时器的id
+    int id2;//定时器的id
 
 private:
     Ui::mainWidgetClass ui;
